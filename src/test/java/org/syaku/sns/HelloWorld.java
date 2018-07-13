bef61ff822937d1ee3f3a284b718ed4a1d6452aa
최석균 <syaku@naver.com>
2018-07-13 21:52:22 +0900

    개발 기본 구성.

---
+++ src/test/java/org/syaku/sns/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.syaku.sns;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @since 2018. 7. 13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+}
